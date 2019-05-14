da118ca18d9d1ee9f0b1805f3d598ee8d96a646c
zhouli <17607619682@163.com>
2019-05-14 11:51:03 +0800

    开奖提醒设置

--- project/src/views/home/openRemind.vue
+++ project/src/views/home/openRemind.vue
@@ -3,12 +3,12 @@
         <title-bar title_name="开奖提醒" />
         <div style="padding:0.2rem;">
             <p style="padding:0.4rem 0;font-size:0.42rem;line-height:1.6;color:#6B6B6B;">设置彩种开奖提醒后，倒计时开奖后将有声音提醒用户查看开奖号码</p>
-            <div class="open_remind_item dis_flex" v-for="(item,k) in list" :key="k">
+            <div class="open_remind_item dis_flex" v-for="(item,k) in list" :key="k"  @click="changeStatus(item)">
                 <div class="flex_grow_1">
                     <img src="../../assets/clock.png" alt="">
                     <span>{{item.lotname}}</span>
                 </div>
-                <van-icon name="checked" :color="item.status?'#FC7953':'#B5B5B5'" @click="changeStatus(item)"/>
+                <van-icon name="checked" :color="item.status?'#FC7953':'#B5B5B5'"/>
             </div>
         </div>
         <div style="padding:0.8rem;">
@@ -62,20 +62,6 @@ export default {
 <style lang="stylus" scoped>
     .container
         background #F5F5F5
-    .plan_list_box
-        width 94%
-        background #ffffff
-        border-radius .2rem
-        margin .2rem auto
-        padding .3rem
-        box-sizing border-box
-        p 
-            line-height .65rem
-        p:first-child
-            color #6D6462
-            font-size .28rem
-        p:last-child
-            color #3B3B3B
     .open_remind_item
         background #fff  
         padding 0.6rem 0.3rem  
