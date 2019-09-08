3f7d09860d303bda8818a136ec5fe5be7ccdbb9e
zhouli121018 <17607619682@163.com>
2019-09-08 16:57:42 +0800

    邀请明细

---
+++ project/src/views/personal/inviteDetail.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="container">
+        <title-bar title_name="推荐赚钱" />
+        <div>
+            <table style="width:100%;" v-if="list.length>0">
+                <tr>
+                    <td class="head_td" style="padding-left:.2rem;">时间</td>
+                    <td class="head_td">被邀人</td>
+                    <td class="head_td">状态</td>
+                </tr>
+                <!-- <tr >
+                    <td style="padding-left:.2rem">2019/01/01 19:19:12</td>
+                    <td>176****9682</td>
+                    <td>已开通会员</td>
+                </tr>
+                <tr >
+                    <td style="padding-left:.2rem">2019/01/01 19:19:12</td>
+                    <td>176****9682</td>
+                    <td>还差一个</td>
+                </tr> -->
+                <tr v-for="(l,index) in list" :key="index">
+                    <td style="padding-left:.2rem">{{l.createtime}}</td>
+                    <td>{{l.username}}</td>
+                    <td>{{l.status}}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+import {getinvitelist } from '@/api'
+export default {
+    data(){
+        return {
+            title_name: '邀请明细',
+            title:'',
+            list:[],
+            content:''
+        }
+    },
+    methods:{
+        async getinvitelist() {
+            let obj = {};
+            if(localStorage.getItem('uid')){
+                obj.uid = localStorage.getItem('uid');
+            }
+            if(localStorage.getItem('sid')){
+                obj.sid = localStorage.getItem('sid');
+            }
+            const { data } = await getinvitelist(obj);
+            this.list = data.list;
+        },
+    },
+    created(){
+        this.getinvitelist();
+    }
+}
+</script>
+
+<style  lang="stylus" scoped>
+table td.head_td{
+    background:#eee;
+    padding:.2rem 0;
+    font-weight:bold;
+    font-size:.42rem;
+}
+table td{
+    padding-top:.2rem;
+    padding-bottom:.2rem;
+    font-size:.4rem
+}
+    
+</style>
